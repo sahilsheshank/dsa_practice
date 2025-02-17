91fca9ad557306a1e6887e6e67615c67d80cb6a6
sahilsheshank <138553429+sahilsheshank@users.noreply.github.com>
2025-02-17 19:47:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Duplicates from unsorted array/remove-duplicates-from-unsorted-array.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> removeDuplicate(vector<int>& arr) {
+        unordered_map<int,int>mpp;
+        for(auto it:arr){
+            mpp[it]++;
+        }
+        vector<int>ans;
+        for(int i=0;i<arr.size();i++){
+            if(mpp.count(arr[i])){
+                ans.push_back(arr[i]);
+                mpp.erase(arr[i]);
+                
+            }
+            else continue;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.removeDuplicate(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
