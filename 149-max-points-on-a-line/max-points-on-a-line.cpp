73adee70831f04711aff05bbbcdb1ec966f8fa6a
sahilsheshank <138553429+sahilsheshank@users.noreply.github.com>
2025-03-11 23:49:52 +0530

    Time: 43 ms (26.34%) | Memory: 24.3 MB (15.07%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/max-points-on-a-line.cpp
@@ -4,24 +4,22 @@ public:
         if(points.size()==1) return 1;
         int res = 0;
         for (int i = 0; i < points.size(); i++) {
-
-            for (int j = i + 1; j < points.size(); j++) {
-                int cnt = 2;
-
-                int dx = points[j][0] - points[i][0];
-                int dy = points[j][1] - points[i][1];
-                for (int k = 0; k < points.size(); k++) {
-                    if (k != i && k != j) {
-                        int dx_ = points[k][0] - points[i][0];
-                        int dy_ = points[k][1] - points[i][1];
-                        if (dy * dx_ == dy_ * dx) {
-                            cnt++;
-                        }
+            unordered_map<double,int>mpp;
+            for (int j = 0; j < points.size(); j++) {
+                if(j!=i){
+                    auto dx=points[j][0]-points[i][0];
+                    auto dy=points[j][1]-points[i][1];
+                    auto theta=atan2(dy,dx);
+                    mpp[theta]++;
+                    if(mpp[theta]+1 > res){
+                        res=mpp[theta]+1;
                     }
                 }
-                res = max(res, cnt);
+                
             }
+            
         }
+    
         return res;
     }
 };
